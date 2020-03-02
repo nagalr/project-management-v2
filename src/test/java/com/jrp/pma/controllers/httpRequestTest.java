08bc109c719de007bf7e4e797a211a9655a1f744
Ronnen Nagal <nagalr@gmail.com>
2020-03-02 14:27:00 -0500

    Fill the httpRequestTest with the relevant fields for testing, create a jar file with Maven

--- src/test/java/com/jrp/pma/controllers/httpRequestTest.java
+++ src/test/java/com/jrp/pma/controllers/httpRequestTest.java
@@ -1,5 +1,29 @@
 package com.jrp.pma.controllers;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // The test will run with this annotation ONLY if the name of the test package will start with 'com.jrp.pma' like here, this test will run on a random Port as we defined in the arguments
 public class httpRequestTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate; // we can mimic everything we can do in a browser with this method
+
+    @Test
+    public void homePageReturnsVersionNumberCorrectly_thenSuccess() {
+        String renderHtml =
+                this.restTemplate.getForObject("http://localhost:" + port +"/", String.class); // restTemplate will have the all webpage, so it's good for testing webpages and such
+        assertEquals(renderHtml.contains("3.3.3"), true);
+    }
 }
