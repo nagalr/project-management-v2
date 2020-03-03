f1fdf902d297a1780cc3a370e545c3ccdfbb754c
Ronnen Nagal <nagalr@gmail.com>
2020-03-03 14:25:09 -0500

    Define the logging package

--- src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -10,15 +10,11 @@ package com.jrp.pma.dao;
  The Test will use application.properties in the Resources here
 */
 
-import com.jrp.pma.ProjectManagementV2Application;
-import com.jrp.pma.dao.IProjectRepository;
 import com.jrp.pma.entities.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
