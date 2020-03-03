7311a9723250b66b70d9c08f0828ff180528aba0
Ronnen Nagal <nagalr@gmail.com>
2020-03-03 14:57:44 -0500

    define the log function

--- src/main/java/com/jrp/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/jrp/pma/logging/ApplicationLoggerAspect.java
@@ -1,16 +1,35 @@
 package com.jrp.pma.logging;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.stereotype.Component;
 
 
 // AAspect is a code that we want to run as a cross cutting concern
 @Aspect
+@Component // we must define @Component so at the scanning it will go to the Spring Context
 public class ApplicationLoggerAspect {
 
     // we define a 'log' variable that we can utilized all over the application
     private final Logger log  = LoggerFactory.getLogger(this.getClass());
+
+    /*
+     Define what the log format should be
+     one way of logging all over the application
+     using @Pointcut we define where the code here should run
+    */
+    @Pointcut("within(com.jrp.pma.controllers..*)")
+    public void definePackagePointcuts() {
+        // empty method to name the location specified in the pointcut (is like location here)
+    }
+
+    // This method will run after the method in the ()
+    @After("definePackagePointcuts()")
+    public void log() {
+        log.debug("---------------------------------");
+    }
 
 }
