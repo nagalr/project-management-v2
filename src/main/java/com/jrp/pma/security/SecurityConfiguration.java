943c8fa9809e8ecae7c7f283bd95f9d31b94538e
Ronnen Nagal <nagalr@gmail.com>
2020-03-04 08:34:20 -0500

    @Override the built-in 'protected void configure' function

--- src/main/java/com/jrp/pma/security/SecurityConfiguration.java
+++ src/main/java/com/jrp/pma/security/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package com.jrp.pma.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 // the @Configuration annotation will add the class to Spring Context
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
 }
