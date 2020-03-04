549cfeb5965d84a837adddf8fccdbeaf685f8672
Ronnen Nagal <nagalr@gmail.com>
2020-03-04 15:11:18 -0500

    Create the 'UserAccount' @Entity class

---
+++ src/main/java/com/jrp/pma/entities/UserAccount.java
@@ -0,0 +1,10 @@
+package com.jrp.pma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_accounts") // to understand to which table we referring
+public class UserAccount {
+
+}
