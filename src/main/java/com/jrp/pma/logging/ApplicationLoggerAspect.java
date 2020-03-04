91497ceec5cbdf731c7ac3087ed77854d33fa6bc
Ronnen Nagal <nagalr@gmail.com>
2020-03-03 19:27:40 -0500

    Change the logging component to @Around so we can see changes before and after the run.

--- src/main/java/com/jrp/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/jrp/pma/logging/ApplicationLoggerAspect.java
@@ -1,10 +1,8 @@
 package com.jrp.pma.logging;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -32,13 +30,28 @@ public class ApplicationLoggerAspect {
 
     // This method will run after the method in the ()
     // A JointPoint Object gives us access to points during the execution of the app
-    @After("definePackagePointcuts()" )
-    public void logAfter(JoinPoint jp) {
+    @Around("definePackagePointcuts()" )
+    public Object logAfter(ProceedingJoinPoint jp) {
+        log.debug("\n \n \n");
+        log.debug("********* Before Method Execution ********** \n {}.{} () with arguments[s] = {}",
+                jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.debug("_______________________________________ \n \n \n");
+
+        Object o = null;
+        try {
+            o = jp.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
         log.debug("\n \n \n");
         log.debug("********* After Method Execution ********** \n {}.{} () with arguments[s] = {}",
                 jp.getSignature().getDeclaringTypeName(),
                 jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
         log.debug("_______________________________________ \n \n \n");
+
+        return o;
 
     }
 
