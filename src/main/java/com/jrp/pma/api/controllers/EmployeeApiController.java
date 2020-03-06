fe3db75cfb72313dba7f2f6967ee7e70d58a4c81
Ronnen Nagal <nagalr@gmail.com>
2020-03-06 11:30:12 -0500

    Creating the api package and EmployeeApiController.java class within to handle requests

---
+++ src/main/java/com/jrp/pma/api/controllers/EmployeeApiController.java
@@ -0,0 +1,23 @@
+package com.jrp.pma.api.controllers;
+
+import com.jrp.pma.dao.IEmployeeRepository;
+import com.jrp.pma.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app-api/employees")
+public class EmployeeApiController {
+
+    @Autowired
+    IEmployeeRepository empRepo;
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+
+    }
+}
